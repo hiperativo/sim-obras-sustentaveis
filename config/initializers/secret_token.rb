eb8b4628696c62d5e9595c880b72eab9e127aa3d
Pedro Maciel <pedrozath@gmail.com>
2013-06-07 18:27:56 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimObrasSustentaveis::Application.config.secret_token = '0d3e7b297e8e473e950f3ddcb1d1639bf76a4b1a1992ecc27a59efb150b245b7aa5288b9af9b2c5f54c8b1e34cc4b7a09576aec07c9b0e6c3e90f964c444d530'
