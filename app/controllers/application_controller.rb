7024b950d89bf604985b385bf2a72eef9d7d0023
Pedro Maciel <pedrozath@gmail.com>
2013-07-01 10:59:29 -0300

    time zones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
 	helper_method :super_admin?
+	around_filter :set_time_zone
 
 	def super_admin?
 		current_admin.role == "administrador"
@@ -16,4 +17,11 @@ class ApplicationController < ActionController::Base
 	def log(admin, action, details)
 		Log.create(admin_id: admin.id, action: action, details: details)
 	end
+
+	private
+
+	def set_time_zone(&block)
+		Time.use_zone("Brasilia", &block)
+	end
+
 end
